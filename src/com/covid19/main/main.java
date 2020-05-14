0bb377599cb35295f3555baf756335e38b0ac1fc
Andra Ristiano <56077597+andrarstn@users.noreply.github.com>
2020-05-15 01:00:49 +0700

    Final version

---
+++ src/com/covid19/main/main.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.covid19.main;
+
+import com.covid19.error.DaerahException;
+import com.covid19.view.DaerahView;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author andra
+ */
+public class main {
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new DaerahView().setVisible(true);
+                } catch (DaerahException ex) {
+                    Logger.getLogger(DaerahView.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+}
--- src/com/covid19/view/DaerahView.java
+++ src/com/covid19/view/DaerahView.java
@@ -48,6 +48,7 @@ public class DaerahView extends javax.swing.JFrame implements ListSelectionListe
         tableModel.setList(dao.getAllDaerah());
         
         initComponents();
+        btnubah.setEnabled(false);
         tabledaerah.getSelectionModel().addListSelectionListener(this);
         tabledaerah.setModel(tableModel);
     }
@@ -101,6 +102,11 @@ public class DaerahView extends javax.swing.JFrame implements ListSelectionListe
             txtodp.setText(model.getOdp()+"");
             txtpdp.setText(model.getPdp()+"");
             txtpositif.setText(model.getPositif()+"");
+      
+            if(!txtnama.equals("")){
+            btnubah.setEnabled(true);
+            btnsimpan.setEnabled(false);
+        }
         }catch(IndexOutOfBoundsException ie){
             
         }
@@ -368,6 +374,8 @@ public class DaerahView extends javax.swing.JFrame implements ListSelectionListe
     private void btnbatalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnbatalActionPerformed
         // TODO add your handling code here:
         controller.reset(this);
+        btnubah.setEnabled(false);
+        btnsimpan.setEnabled(true);
     }//GEN-LAST:event_btnbatalActionPerformed
 
     private void jLabel8MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseEntered
@@ -397,42 +405,6 @@ public class DaerahView extends javax.swing.JFrame implements ListSelectionListe
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DaerahView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new DaerahView().setVisible(true);
-                } catch (DaerahException ex) {
-                    Logger.getLogger(DaerahView.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnbatal;
